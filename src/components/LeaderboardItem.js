e6c14fe7c03e90480efb672190ae8bc4a9be1634
adolfjeremy <adolf.jer@gmail.com>
2022-12-08 23:09:32 +0700

    integrate data to the component

--- src/components/LeaderboardItem.js
+++ src/components/LeaderboardItem.js
@@ -1,16 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Avatar from "./Avatar";
 
-function LeaderboardItem() {
+function LeaderboardItem({ user, score }) {
     return (
         <div className="leaderboard-item">
             <div className="leaderboard-item__user-info">
-                <Avatar image="https://ui-avatars.com/api/?name=Elon%20musk&background=random" />
-                <h3>Elon Musk</h3>
+                <Avatar image={user.avatar} />
+                <h3>{user.name}</h3>
             </div>
-            <p>470</p>
+            <p>{score}</p>
         </div>
     );
 }
+
+const userShape = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+};
+const leaderboardShape = {
+    score: PropTypes.number.isRequired,
+    user: PropTypes.shape(userShape).isRequired,
+};
+
+LeaderboardItem.propTypes = {
+    ...leaderboardShape,
+};
+
+export { leaderboardShape };
 
 export default LeaderboardItem;
