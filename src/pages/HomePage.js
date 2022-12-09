6f5b1d29552f4df75a777be8562270311b0f719d
adolfjeremy <adolf.jer@gmail.com>
2022-12-09 23:37:28 +0700

    use threads state in homepage and console it, to make sure if its work

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,18 +4,21 @@ import HeaderBar from "../components/HeaderBar";
 import Sidebar from "../components/Sidebar";
 import MainContent from "../components/MainContent";
 import { asyncSetLeaderboard } from "../states/leaderboards/action";
+import { asyncReceiveThreads } from "../states/threads/action";
 
 function HomePage() {
-    const { leaderboards = [] } = useSelector((states) => states);
+    const { leaderboards = [], threads = [] } = useSelector((states) => states);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(asyncSetLeaderboard());
+        dispatch(asyncReceiveThreads());
     }, [dispatch]);
 
     // eslint-disable-next-line no-console
-    console.log(leaderboards);
+    console.log(threads);
+
     return (
         <>
             <HeaderBar />
