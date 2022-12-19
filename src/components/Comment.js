96358ee0a83610919521675fac5e79e3d7e58959
adolfjeremy <adolf.jer@gmail.com>
2022-12-19 22:57:30 +0700

    create neutralize vote comment functionality

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-one-expression-per-line */
 import React from "react";
 import PropTypes from "prop-types";
 import parse from "html-react-parser";
@@ -6,26 +5,58 @@ import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 import VoteButton from "./VoteButton";
 import Avatar from "./Avatar";
 import postedAt from "../utils";
+import { authUserShape } from "./HeaderBar";
 
-// eslint-disable-next-line object-curly-newline
-function Comment({ threadId, comment, upVotesComment, downVotesComment }) {
+function Comment({
+    threadId,
+    comment,
+    upVotesComment,
+    downVotesComment,
+    authUser,
+    neutralizeVotesComment,
+}) {
     const handleUpVotesComment = () => {
         upVotesComment({ threadId, commentId: comment.id });
     };
     const handleDownVotes = () => {
         downVotesComment({ threadId, commentId: comment.id });
     };
+    const handleNeutralizeVotes = () => {
+        neutralizeVotesComment({ threadId, commentId: comment.id });
+    };
     return (
         <div className="comment-item" key={comment.id}>
             <div className="thread-item-vote">
                 <div className="thread-item-vote__buttons">
                     <span>{comment.upVotesBy.length}</span>
-                    <VoteButton handleOnClick={handleUpVotesComment}>
+                    <VoteButton
+                        className={
+                            comment.upVotesBy.includes(authUser.id)
+                                ? "voted"
+                                : ""
+                        }
+                        handleOnClick={
+                            comment.upVotesBy.includes(authUser.id)
+                                ? handleNeutralizeVotes
+                                : handleUpVotesComment
+                        }
+                    >
                         <AiOutlineArrowUp />
                     </VoteButton>
                 </div>
                 <div className="thread-item-vote__buttons">
-                    <VoteButton handleOnClick={handleDownVotes}>
+                    <VoteButton
+                        className={
+                            comment.downVotesBy.includes(authUser.id)
+                                ? "voted"
+                                : ""
+                        }
+                        handleOnClick={
+                            comment.downVotesBy.includes(authUser.id)
+                                ? handleNeutralizeVotes
+                                : handleDownVotes
+                        }
+                    >
                         <AiOutlineArrowDown />
                     </VoteButton>
                     <span>{comment.downVotesBy.length}</span>
@@ -33,7 +64,7 @@ function Comment({ threadId, comment, upVotesComment, downVotesComment }) {
             </div>
             <div className="comment-item-content">
                 <div className="comment-item-content__header">
-                    <Avatar image="https://ui-avatars.com/api/?name=Dimas%20Saputra&background=random" />
+                    <Avatar image={comment.owner.avatar} />
                     <span className="username">{comment.owner.name}</span>
                     <span>{postedAt(comment.createdAt)}</span>
                 </div>
@@ -63,6 +94,8 @@ Comment.propTypes = {
     comment: PropTypes.shape(commentsShape),
     upVotesComment: PropTypes.func.isRequired,
     downVotesComment: PropTypes.func.isRequired,
+    authUser: PropTypes.shape(authUserShape),
+    neutralizeVotesComment: PropTypes.func.isRequired,
 };
 
 export default Comment;
