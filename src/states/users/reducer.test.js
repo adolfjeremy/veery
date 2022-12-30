c6f62285737efb6ce785f2e1a61cfd7daed7a543
adolfjeremy <adolf.jer@gmail.com>
2022-12-31 01:06:35 +0700

    create usersReducer test

---
+++ src/states/users/reducer.test.js
@@ -0,0 +1,48 @@
+import usersReducer from "./reducer";
+
+/**
+ * test scenario for users reducer
+ * - threadsReducer
+ *  - should return the initial state when given by unknown action
+ *  - should return the users when given by RECEIVE_USER action
+ */
+
+describe("usersReducer function", () => {
+    it("should return the initial state when given by unknown action", () => {
+        const initialState = [];
+
+        const action = { type: "UNKNOWN" };
+
+        const nextState = usersReducer(initialState, action);
+
+        expect(nextState).toEqual(initialState);
+    });
+
+    it("should return the users when given by RECEIVE_USER action", () => {
+        const initialState = [];
+
+        const action = {
+            type: "RECEIVE_USER",
+            payload: {
+                users: [
+                    {
+                        id: "john_doe",
+                        name: "John Doe",
+                        email: "john@example.com",
+                        avatar: "https://generated-image-url.jpg",
+                    },
+                    {
+                        id: "jane_doe",
+                        name: "Jane Doe",
+                        email: "jane@example.com",
+                        avatar: "https://generated-image-url.jpg",
+                    },
+                ],
+            },
+        };
+
+        const nextState = usersReducer(initialState, action);
+
+        expect(nextState).toEqual(action.payload.users);
+    });
+});
